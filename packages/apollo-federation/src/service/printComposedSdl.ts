e07000f734b7d6bc1bb87dcaf8ee89e22b7ccf0c
Trevor Scheer <trevor.scheer@gmail.com>
2020-07-20 15:17:27 -0700

    Initial commit for composition work
    This commit shows the diff necessary to fork printSchema to achieve
    parity with our existing printFederatedSchema implementation.
    
    The purpose of this first step is to update our fork using the most
    current version of printSchema, since it's changed over time.
    
    In future commits, this new function will diverge from the existing
    implementation in order to implement a new composition format.
    
    Note: newly introduced test cases are in place to validate work in an
    iterative manner and are subject to updates and/or deletion.

--- packages/apollo-federation/src/service/printComposedSdl.ts
+++ packages/apollo-federation/src/service/printComposedSdl.ts
@@ -1,42 +1,36 @@
-// @flow strict
-
-import objectValues from '../polyfills/objectValues';
-
-import inspect from '../jsutils/inspect';
-import invariant from '../jsutils/invariant';
-
-import { print } from '../language/printer';
-import { printBlockString } from '../language/blockString';
-
-import type { GraphQLSchema } from '../type/schema';
-import type { GraphQLDirective } from '../type/directives';
-import type {
-  GraphQLNamedType,
-  GraphQLScalarType,
-  GraphQLEnumType,
-  GraphQLObjectType,
-  GraphQLInterfaceType,
-  GraphQLUnionType,
-  GraphQLInputObjectType,
-} from '../type/definition';
-import { isIntrospectionType } from '../type/introspection';
-import { GraphQLString, isSpecifiedScalarType } from '../type/scalars';
 import {
-  DEFAULT_DEPRECATION_REASON,
+  GraphQLSchema,
   isSpecifiedDirective,
-} from '../type/directives';
-import {
+  isIntrospectionType,
+  isSpecifiedScalarType,
+  GraphQLNamedType,
+  GraphQLDirective,
   isScalarType,
   isObjectType,
   isInterfaceType,
   isUnionType,
   isEnumType,
   isInputObjectType,
-} from '../type/definition';
+  GraphQLScalarType,
+  GraphQLObjectType,
+  GraphQLInterfaceType,
+  GraphQLUnionType,
+  GraphQLEnumType,
+  GraphQLInputObjectType,
+  GraphQLArgument,
+  GraphQLInputField,
+  astFromValue,
+  print,
+  GraphQLField,
+  GraphQLEnumValue,
+  GraphQLString,
+  DEFAULT_DEPRECATION_REASON,
+  FieldDefinitionNode,
+  ASTNode,
+} from 'graphql';
+import { Maybe } from '../composition';
 
-import { astFromValue } from './astFromValue';
-
-type Options = {|
+type Options = {
   /**
    * Descriptions are defined as preceding string literals, however an older
    * experimental version of the SDL supported preceding comments as
@@ -45,8 +39,8 @@ type Options = {|
    *
    * Default: false
    */
-  commentDescriptions?: boolean,
-|};
+  commentDescriptions?: boolean;
+};
 
 /**
  * Accepts options as a second argument:
@@ -55,7 +49,7 @@ type Options = {|
  *        Provide true to use preceding comments as the description.
  *
  */
-export function printSchema(schema: GraphQLSchema, options?: Options): string {
+export function printComposedSdl(schema: GraphQLSchema, options?: Options): string {
   return printFilteredSchema(
     schema,
     (n) => !isSpecifiedDirective(n),
@@ -84,10 +78,10 @@ function printFilteredSchema(
   schema: GraphQLSchema,
   directiveFilter: (type: GraphQLDirective) => boolean,
   typeFilter: (type: GraphQLNamedType) => boolean,
-  options,
+  options?: Options,
 ): string {
   const directives = schema.getDirectives().filter(directiveFilter);
-  const types = objectValues(schema.getTypeMap()).filter(typeFilter);
+  const types = Object.values(schema.getTypeMap()).filter(typeFilter);
 
   return (
     [printSchemaDefinition(schema)]
@@ -100,7 +94,7 @@ function printFilteredSchema(
   );
 }
 
-function printSchemaDefinition(schema: GraphQLSchema): ?string {
+function printSchemaDefinition(schema: GraphQLSchema): string | undefined {
   if (schema.description == null && isSchemaOfCommonNames(schema)) {
     return;
   }
@@ -179,11 +173,10 @@ export function printType(type: GraphQLNamedType, options?: Options): string {
     return printInputObject(type, options);
   }
 
-  // istanbul ignore next (Not reachable. All possible types have been considered)
-  invariant(false, 'Unexpected type: ' + inspect((type: empty)));
+  throw Error('Unexpected type: ' + (type as GraphQLNamedType).toString());
 }
 
-function printScalar(type: GraphQLScalarType, options): string {
+function printScalar(type: GraphQLScalarType, options?: Options): string {
   return (
     printDescription(options, type) +
     `scalar ${type.name}` +
@@ -200,16 +193,30 @@ function printImplementedInterfaces(
     : '';
 }
 
-function printObject(type: GraphQLObjectType, options): string {
+function printObject(type: GraphQLObjectType, options?: Options): string {
   return (
     printDescription(options, type) +
     `type ${type.name}` +
     printImplementedInterfaces(type) +
+    // Federation addition for @key
+    printKeyDirectives(type) +
     printFields(options, type)
   );
 }
 
-function printInterface(type: GraphQLInterfaceType, options): string {
+function printKeyDirectives(type: GraphQLObjectType): string {
+  const metadata = type.extensions?.federation;
+  if (!metadata) return '';
+
+  const { serviceName, keys } = metadata;
+  if (!keys) return '';
+
+  return keys[serviceName].map(
+    (key: FieldDefinitionNode[]) => ` @key(fields: "${key.map(print)}")`,
+  ).join(" ");
+}
+
+function printInterface(type: GraphQLInterfaceType, options?: Options): string {
   return (
     printDescription(options, type) +
     `interface ${type.name}` +
@@ -218,13 +225,13 @@ function printInterface(type: GraphQLInterfaceType, options): string {
   );
 }
 
-function printUnion(type: GraphQLUnionType, options): string {
+function printUnion(type: GraphQLUnionType, options?: Options): string {
   const types = type.getTypes();
   const possibleTypes = types.length ? ' = ' + types.join(' | ') : '';
   return printDescription(options, type) + 'union ' + type.name + possibleTypes;
 }
 
-function printEnum(type: GraphQLEnumType, options): string {
+function printEnum(type: GraphQLEnumType, options?: Options): string {
   const values = type
     .getValues()
     .map(
@@ -240,8 +247,11 @@ function printEnum(type: GraphQLEnumType, options): string {
   );
 }
 
-function printInputObject(type: GraphQLInputObjectType, options): string {
-  const fields = objectValues(type.getFields()).map(
+function printInputObject(
+  type: GraphQLInputObjectType,
+  options?: Options,
+): string {
+  const fields = Object.values(type.getFields()).map(
     (f, i) =>
       printDescription(options, f, '  ', !i) + '  ' + printInputValue(f),
   );
@@ -250,8 +260,11 @@ function printInputObject(type: GraphQLInputObjectType, options): string {
   );
 }
 
-function printFields(options, type) {
-  const fields = objectValues(type.getFields()).map(
+function printFields(
+  options: Options | undefined,
+  type: GraphQLObjectType | GraphQLInterfaceType,
+) {
+  const fields = Object.values(type.getFields()).map(
     (f, i) =>
       printDescription(options, f, '  ', !i) +
       '  ' +
@@ -259,16 +272,42 @@ function printFields(options, type) {
       printArgs(options, f.args, '  ') +
       ': ' +
       String(f.type) +
-      printDeprecated(f),
+      printDeprecated(f) +
+      printFederationFieldDirectives(f),
   );
   return printBlock(fields);
 }
 
-function printBlock(items) {
+export function printWithReducedWhitespace(ast: ASTNode): string {
+  return print(ast)
+    .replace(/\s+/g, ' ')
+    .trim();
+}
+
+function printFederationFieldDirectives(field: GraphQLField<any, any>): string {
+  if (!field.extensions?.federation) return "";
+
+  let printed = "";
+  const { provides = [], requires = [] } = field.extensions.federation;
+  if (provides.length > 0) {
+    printed += ` @provides(fields: "${provides.map(printWithReducedWhitespace).join(" ")}")`;
+  }
+  if (requires.length > 0) {
+    printed += ` @requires(fields: "${requires.map(printWithReducedWhitespace).join(" ")}")`;
+  }
+
+  return printed;
+}
+
+function printBlock(items: string[]) {
   return items.length !== 0 ? ' {\n' + items.join('\n') + '\n}' : '';
 }
 
-function printArgs(options, args, indentation = '') {
+function printArgs(
+  options: Options | undefined,
+  args: GraphQLArgument[],
+  indentation = '',
+) {
   if (args.length === 0) {
     return '';
   }
@@ -295,7 +334,7 @@ function printArgs(options, args, indentation = '') {
   );
 }
 
-function printInputValue(arg) {
+function printInputValue(arg: GraphQLInputField) {
   const defaultAST = astFromValue(arg.defaultValue, arg.type);
   let argDecl = arg.name + ': ' + String(arg.type);
   if (defaultAST) {
@@ -304,7 +343,7 @@ function printInputValue(arg) {
   return argDecl;
 }
 
-function printDirective(directive, options) {
+function printDirective(directive: GraphQLDirective, options?: Options) {
   return (
     printDescription(options, directive) +
     'directive @' +
@@ -316,13 +355,15 @@ function printDirective(directive, options) {
   );
 }
 
-function printDeprecated(fieldOrEnumVal) {
-  const { deprecationReason } = fieldOrEnumVal;
-  if (deprecationReason == null) {
+function printDeprecated(
+  fieldOrEnumVal: GraphQLField<any, any> | GraphQLEnumValue,
+) {
+  if (!fieldOrEnumVal.isDeprecated) {
     return '';
   }
-  const reasonAST = astFromValue(deprecationReason, GraphQLString);
-  if (reasonAST && deprecationReason !== DEFAULT_DEPRECATION_REASON) {
+  const reason = fieldOrEnumVal.deprecationReason;
+  const reasonAST = astFromValue(reason, GraphQLString);
+  if (reasonAST && reason !== DEFAULT_DEPRECATION_REASON) {
     return ' @deprecated(reason: ' + print(reasonAST) + ')';
   }
   return ' @deprecated';
@@ -334,16 +375,19 @@ function printSpecifiedByUrl(scalar: GraphQLScalarType) {
   }
   const url = scalar.specifiedByUrl;
   const urlAST = astFromValue(url, GraphQLString);
-  invariant(
-    urlAST,
-    'Unexpected null value returned from `astFromValue` for specifiedByUrl',
-  );
+
+  if (!urlAST) {
+    throw new Error(
+      'Unexpected null value returned from `astFromValue` for specifiedByUrl',
+    );
+  }
+
   return ' @specifiedBy(url: ' + print(urlAST) + ')';
 }
 
-function printDescription(
-  options,
-  def,
+function printDescription<T extends { description?: Maybe<string> }>(
+  options: Options | undefined,
+  def: T,
   indentation = '',
   firstInBlock = true,
 ): string {
@@ -364,7 +408,11 @@ function printDescription(
   return prefix + blockString.replace(/\n/g, '\n' + indentation) + '\n';
 }
 
-function printDescriptionWithComments(description, indentation, firstInBlock) {
+function printDescriptionWithComments(
+  description: string,
+  indentation: string,
+  firstInBlock: boolean,
+) {
   const prefix = indentation && !firstInBlock ? '\n' : '';
   const comment = description
     .split('\n')
@@ -372,4 +420,39 @@ function printDescriptionWithComments(description, indentation, firstInBlock) {
     .join('\n');
 
   return prefix + comment + '\n';
+}
+
+/**
+ * Print a block string in the indented block form by adding a leading and
+ * trailing blank line. However, if a block string starts with whitespace and is
+ * a single-line, adding a leading blank line would strip that whitespace.
+ *
+ * @internal
+ */
+export function printBlockString(
+  value: string,
+  indentation: string = '',
+  preferMultipleLines: boolean = false,
+): string {
+  const isSingleLine = value.indexOf('\n') === -1;
+  const hasLeadingSpace = value[0] === ' ' || value[0] === '\t';
+  const hasTrailingQuote = value[value.length - 1] === '"';
+  const hasTrailingSlash = value[value.length - 1] === '\\';
+  const printAsMultipleLines =
+    !isSingleLine ||
+    hasTrailingQuote ||
+    hasTrailingSlash ||
+    preferMultipleLines;
+
+  let result = '';
+  // Format a multi-line block quote to account for leading space.
+  if (printAsMultipleLines && !(isSingleLine && hasLeadingSpace)) {
+    result += '\n' + indentation;
+  }
+  result += indentation ? value.replace(/\n/g, '\n' + indentation) : value;
+  if (printAsMultipleLines) {
+    result += '\n';
+  }
+
+  return '"""' + result.replace(/"""/g, '\\"""') + '"""';
 }
